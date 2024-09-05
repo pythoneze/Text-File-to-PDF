418bfe026b9dd3f8118ad198850757d7b64ef4df
pythoneze <pythoneze@gmail.com>
2024-09-05 15:16:19 -0400

    Add content to the pages

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-from fpdf import FPDF
+import pandas as pd
 import glob as gb
+from fpdf import FPDF
 from pathlib import Path
 
 FILEPATHS = gb.glob("text_files/*.txt")
 
 pdf = FPDF(orientation="P", unit="mm", format="A4")
 
-for filepath in FILEPATHS:        
+
+def create_pdf(filepath, content):
     pdf.add_page()
 
     filename = Path(filepath).stem
@@ -14,5 +16,19 @@ for filepath in FILEPATHS:
 
     pdf.set_font(family="Times", size=16, style="B")
     pdf.cell(w=50, h=8, txt=header, ln=1)
+    pdf.set_font(family="Arial", size=12)
+    pdf.multi_cell(w=0, h=5, txt=content, border=0)
 
-pdf.output("animals.pdf")
+
+def create_and_save_pdfs():
+    for filepath in FILEPATHS:
+        with open(filepath, "r") as file:
+            content = file.read()
+
+        create_pdf(filepath, content)
+
+    pdf.output("animals.pdf")
+
+
+if __name__ == "__main__":
+    create_and_save_pdfs()
